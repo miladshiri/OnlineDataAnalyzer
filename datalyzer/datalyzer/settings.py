802d22297e64a86ebc21ff68bda19b02f9cf973c
milad <miladonlinemail@gmail.com>
2019-06-02 01:29:52 +0430

    Fix redirect urls

--- datalyzer/datalyzer/settings.py
+++ datalyzer/datalyzer/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 LOGIN_URL = '/user/signin/'
-
+LOGIN_REDIRECT_URL = 'main:home'
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
--- datalyzer/userprofile/urls.py
+++ datalyzer/userprofile/urls.py
@@ -8,5 +8,6 @@ app_name = 'userprofile'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', auth_views.LoginView.as_view(template_name='userprofile/signin.html'), name='signin'),
+    path('signout/', auth_views.LogoutView.as_view(next_page='main:home'), name='signout')
 ]
 
